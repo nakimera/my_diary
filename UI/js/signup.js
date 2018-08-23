fbd2fe468e29334424a2fd245b7aa744c793899f
nakimera <prossienakimera@gmail.com>
2018-08-23 20:11:54 +0300

    edit signup page

---
+++ UI/js/signup.js
@@ -0,0 +1,24 @@
+function registerUser() {
+    username = document.getElementById('username').value;
+    email_address = document.getElementById('email').value;
+    password = document.getElementById('password').value;
+
+    user = {
+        username: username,
+        email_address: email_address,
+        password: password
+    }
+
+    fetch('http://127.0.0.1:5000/api/v1/auth/signup', 
+    {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(user)
+    })
+    .then(response => {
+        status_code  = response.status
+        return response.json()
+    })
+    
+    .catch(error => alert(error))
+}
