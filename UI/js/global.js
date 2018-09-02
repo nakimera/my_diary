495e1a3a136d8f44c8c5263f71200ea81a86a76e
nakimera <prossienakimera@gmail.com>
2018-09-02 09:01:08 +0300

    change host

--- UI/js/global.js
+++ UI/js/global.js
@@ -1,5 +1,5 @@
-const host = 'http://127.0.0.1:5000'
-//const host = 'https://my-diary-app-np.herokuapp.com'
+// const host = 'http://127.0.0.1:5000'
+const host = 'https://my-diary-app-np.herokuapp.com'
 
 function logOut() {
     window.localStorage.clear();
